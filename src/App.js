b95979b7fe30692f3d361465ff7fc87bf3161b77
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-11 15:01:06 +0100

    Part-4 get image URLs and map to display

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>React Photo Viewer</h1>
-      <PhotoViewer src={"https://picsum.photos/id/600/1600/900.jpg"}/>
+      <PhotoViewer />
     </div>
   );
 }
--- src/PhotoViewer/PhotoViewer.js
+++ src/PhotoViewer/PhotoViewer.js
@@ -1,9 +1,35 @@
-import React from "react";  // import React (to provide access to JSX)
+import React, { useState } from "react";  // import React (to provide access to JSX)
 
-export function PhotoViewer({src}) {    // declare a new function called 'PhotoViewer'
-    return (                
-        <div className="image-container">               
-            <img className = "image" src={src}/>
+export function PhotoViewer() {
+    const urls = getImageUrls();
+    const [selectedImage, setSelectedImage] = useState("")
+    const images = urls.map((url,k) => <img key={k} className="image" src={url} onClick={() => setSelectedImage(url)}/>);
+
+    return (
+        <div className="image-container">
+            {(!selectedImage) ? <p>no image selected</p>
+                : <img key="selectedImage" className="image" src={selectedImage}/>
+            }
+            {images}
+           
         </div>
     );
 }
+
+function getImageUrls() {
+    const brokenImages = [
+        1, 24, 32, 36, 44, 47
+    ];
+
+    const urls = [];
+
+    for (let i = 0; i < 50; i++) {
+        if (!brokenImages.includes(i)) {
+            const imageNumberString = i.toString().padStart(2, '0');
+            urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/900.jpg`)
+        }
+    }
+    console.log(urls);
+    return urls;
+}
+
