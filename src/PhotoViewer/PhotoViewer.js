2846e14d240f5881d2255b7d15db7ac4adefc4ae
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-11 12:04:28 +0100

    finished Part-2

--- src/PhotoViewer/PhotoViewer.js
+++ src/PhotoViewer/PhotoViewer.js
@@ -2,8 +2,8 @@ import React from "react";  // import React (to provide access to JSX)
 
 export function PhotoViewer() {    // declare a new function called 'PhotoViewer'
     return (                
-        <div>               
-            Hello World!
+        <div className="image-container">               
+            <img className = "image" src="https://picsum.photos/300/200"/>
         </div>
     );
 }
