46043947de9e45d7e2e7d40a3a5063d9d3b412b1
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-11 11:23:34 +0100

    created PhotoViewer.js

---
+++ src/PhotoViewer/PhotoViewer.js
@@ -0,0 +1,9 @@
+import React from "react";  // import React (to provide access to JSX)
+
+export function PhotoViewer() {    // declare a new function called 'PhotoViewer'
+    return (                
+        <div>               
+            Hello World!
+        </div>
+    );
+}
