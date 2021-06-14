e26e3f299db07953bea6fa3f0202957288c32c54
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-14 15:54:17 +0100

    Added Snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {PhotoViewer} from './PhotoViewer/PhotoViewer';
+import { PhotoViewer } from './PhotoViewer/PhotoViewer';
 import renderer from 'react-test-renderer';
 
 test('should display header', () => {
@@ -11,10 +11,36 @@ test('should display header', () => {
 
 test('should show images', () => {
   const selectedImage = "any string will do.com";
-  const setSelectedImage = () => {};
-  render(<PhotoViewer setSelectedImage={setSelectedImage} selectedImage={selectedImage}/>);
+  const setSelectedImage = () => { };
+  render(<PhotoViewer setSelectedImage={setSelectedImage} selectedImage={selectedImage} />);
   const element = screen.getByTestId('selected_image');
   expect(element).toBeInTheDocument();
 });
 
+test("doesn't include broken images", () => {
+  const selectedImage = "any string will do.com";
+  const setSelectedImage = () => { };
+  render(<PhotoViewer setSelectedImage={setSelectedImage} selectedImage={selectedImage} />);
+  const element = screen.getByTestId('selected_image');
+  expect(element).toBeInTheDocument();
+});
+
+describe("Photoviewer", () => {
+  it("Should match snapshot if image is not selected", async () => {
+    const selectedImage = "";
+    const setSelectedImage = () => { };
+    const tree = renderer.create(<PhotoViewer setSelectedImage={setSelectedImage} selectedImage={selectedImage}/>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  }); 
+
+  it("Should match snapshot if image is selected", async () => {
+    const selectedImage = "any string will do.com";
+    const setSelectedImage = () => { };
+    const tree = renderer.create(<PhotoViewer setSelectedImage={setSelectedImage} selectedImage={selectedImage}/>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  }); 
+  
+});
 
